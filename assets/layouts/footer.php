f6be0f8db03f4fe2a1c18672027b3d6a38168b94
Jitendra Kumar <jitendrasahu6761@gmail.com>
2023-03-17 16:35:17 +0530

    Bootstrap url are updated local to internet

--- assets/layouts/footer.php
+++ assets/layouts/footer.php
@@ -1,7 +1,8 @@
     <!-- FOOTER WORKING  -->
-    <!-- <script src="../assets/vendor/js/jquery-3.4.1.min.js"></script> -->
-    <!-- <script src="../assets/vendor/js/popper.min.js"></script> -->
-    <script src="../assets/vendor/bootstrap-5.2.0-beta1-dist/js/bootstrap.min.js"></script>
+   
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+
+    <!-- <script src="../assets/vendor/bootstrap-5.2.0-beta1-dist/js/bootstrap.min.js"></script> -->
 
     </body>
 
--- assets/layouts/header.php
+++ assets/layouts/header.php
@@ -10,8 +10,11 @@ require '../assets/setup/connection.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo TITLE . ' | ' . APP_NAME; ?></title>
-    <link rel="stylesheet" href="../assets/vendor/bootstrap-5.2.0-beta1-dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../assets/vendor/fontawesome-5.12.0/css/all.min.css">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+    <!-- <link rel="stylesheet" href="../assets/vendor/bootstrap-5.2.0-beta1-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/vendor/fontawesome-5.12.0/css/all.min.css"> -->
 </head>
 <body>
     <header>
